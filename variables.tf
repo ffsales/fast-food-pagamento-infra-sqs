da5d188cd994670e631abd5ff44bbbd99bba7b35
Felipe Ferreira de Sales <felipe.ferreira.sales@gmail.com>
2024-01-24 07:40:50 -0300

    cria nome de filas

--- variables.tf
+++ variables.tf
@@ -3,7 +3,17 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "sqs_name" {
+variable "sqs_pagamento_name" {
   description = "Name of the sqs queue to be created"
   default     = "notificacao-pagamento-sync"
+}
+
+variable "sqs_producao_name" {
+  description = "Name of the sqs queue to be created"
+  default     = "notificacao-criacao-pedido"
+}
+
+variable "sqs_producao_name" {
+  description = "Name of the sqs queue to be created"
+  default     = "notificacao-status-pedido"
 }
